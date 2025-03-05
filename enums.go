8b8b0e0ecdd35d804cfbdc3e3e224826e7715b49
AleAccurso <aleaccurso@hotmail.com>
2025-03-06 00:23:20 +0100

    enum map converter

--- enums.go
+++ enums.go
@@ -1,18 +1,26 @@
 package mytools
 
 import (
+	"fmt"
 	"reflect"
 )
 
 // MapEnum takes a map of interface_a to interface_b and a value of interface_a,
 // then returns the corresponding interface_b value
 func MapEnum(mapping map[interface{}]interface{}, value interface{}) interface{} {
-	// Check if mapping is nil
-	if mapping == nil {
-		return nil
+	// Get target type
+	var targetType reflect.Type
+	for _, v := range mapping {
+		targetType = reflect.TypeOf(v)
+		break
 	}
 
-	// Check if the value exists in the map
+	strValue := fmt.Sprintf("%v", value)
+
+	if mapping == nil {
+		return reflect.ValueOf(strValue).Convert(targetType).Interface()
+	}
+
 	for k, v := range mapping {
 		// Use reflect.DeepEqual to compare interfaces since direct comparison might not work
 		if reflect.DeepEqual(k, value) {
@@ -20,5 +28,5 @@ func MapEnum(mapping map[interface{}]interface{}, value interface{}) interface{}
 		}
 	}
 
-	return nil
+	return reflect.ValueOf(strValue).Convert(targetType).Interface()
 }
